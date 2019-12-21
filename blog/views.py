280570a1b065961aaece5387fe0aef2b694c1635
phannhien <phanhien1234@gmail.com>
2019-12-22 02:54:38 +0700

    load toan bo bai viet len

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
-
+from .models import Post
+from django.http import HttpResponse
 # Create your views here.
+def list(request):
+    Data = {'Posts': Post.objects.all().order_by("date")}
+    return render(request, 'blog/blog.html', Data)
