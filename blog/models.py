18cdb068344411c350ac0559708ff453c40a37a1
phannhien <phanhien1234@gmail.com>
2019-12-21 23:24:42 +0700

    cap nhat chuc nang cua admin

--- blog/models.py
+++ blog/models.py
@@ -5,3 +5,5 @@ class Post(models.Model):
     title = models.CharField(max_length=100)
     body = models.TextField()
     date = models.DateTimeField(auto_now_add=True)
+    def __str__(self):
+        return self.title
