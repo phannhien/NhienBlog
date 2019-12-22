99ccce95e3c4e097713400fe009d3471a4078c68
phannhien <phanhien1234@gmail.com>
2019-12-22 15:29:07 +0700

    them anh vao blog

--- NhienBlog/settings.py
+++ NhienBlog/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '@^m^v+ym=+3nioobl=b(7jo3a@2!2vzfja(yqz$*mdd^!9w1-8'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
@@ -120,3 +120,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR,'media')
--- blog/models.py
+++ blog/models.py
@@ -6,3 +6,4 @@ class Post(models.Model):
     title = models.CharField(max_length=100)
     body = models.TextField()
     date = models.DateTimeField(auto_now_add=True)
+    image = models.ImageField(null=True)
