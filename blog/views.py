ee23058219440219506084031bbf3aa86e17ee3d
phannhien <phanhien1234@gmail.com>
2019-12-22 14:58:34 +0700

    Cap nhat ngay 22-12

--- blog/views.py
+++ blog/views.py
@@ -5,3 +5,6 @@ from django.http import HttpResponse
 def list(request):
     Data = {'Posts': Post.objects.all().order_by("date")}
     return render(request, 'blog/blog.html', Data)
+def post(request, id):
+    post = Post.objects.get(id=id)
+    return render(request,'blog/post.html', {'post': post})
